371b4280d74762e1e74849f053b84d1d475cb7eb
VitorZillig <72581458+VitorZillig@users.noreply.github.com>
2024-05-26 20:00:57 -0300

    Update Origem.cpp

--- M3 - Adicionando Texturas/Exericio8/Origem.cpp
+++ M3 - Adicionando Texturas/Exericio8/Origem.cpp
@@ -1,11 +1,12 @@
-/* Hello Triangle - código adaptado de https://learnopengl.com/#!Getting-started/Hello-Triangle
+/* Hello Triangle - cÃ³digo adaptado de https://learnopengl.com/#!Getting-started/Hello-Triangle
  *
  * Adaptado por Rossana Baptista Queiroz
- * para a disciplina de Processamento Gráfico - Jogos Digitais - Unisinos
- * Versão inicial: 7/4/2017
- * Última atualização em 12/05/2023
+ * para a disciplina de Processamento GrÃ¡fico - Jogos Digitais - Unisinos
+ * VersÃ£o inicial: 7/4/2017
+ * Ãšltima atualizaÃ§Ã£o em 12/05/2023
  *
  */
+//Nome: Vitor Henrique Grego Zillig
 
 #include <iostream>
 #include <string>
@@ -31,19 +32,19 @@ using namespace std;
 #include "../../M3 - Adicionando Texturas/stb_image.h"
 
 
-// Protótipo da função de callback de teclado
+// ProtÃ³tipo da funÃ§Ã£o de callback de teclado
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-// Protótipos das funções
+// ProtÃ³tipos das funÃ§Ãµes
 int setupShader();
 int loadSimpleOBJ(string filepath, int& nVerts, glm::vec3 color);
 int loadTexture(string path);
 string getTextureName(string filepath);
 
-// Dimensões da janela (pode ser alterado em tempo de execução)
+// DimensÃµes da janela (pode ser alterado em tempo de execuÃ§Ã£o)
 const GLuint WIDTH = 1000, HEIGHT = 1000;
 
-// Código fonte do Vertex Shader (em GLSL): ainda hardcoded
+// CÃ³digo fonte do Vertex Shader (em GLSL): ainda hardcoded
 const GLchar* vertexShaderSource = "#version 450\n"
 "layout (location = 0) in vec3 position;\n"
 "layout (location = 1) in vec3 color;\n"
@@ -53,7 +54,7 @@ const GLchar* vertexShaderSource = "#version 450\n"
 "out vec2 texCoord;\n"
 "void main()\n"
 "{\n"
-//adicionado ao shader o posicionamento de um segundo cubo por meio do "gl_InstanceID, caso necessário"
+//adicionado ao shader o posicionamento de um segundo cubo por meio do "gl_InstanceID, caso necessÃ¡rio"
 "vec3 pos = position;\n"
 "pos.y -= float(gl_InstanceID) * 1.2;\n"	
 "gl_Position = model * vec4(pos, 1.0);\n"
@@ -61,7 +62,7 @@ const GLchar* vertexShaderSource = "#version 450\n"
 "texCoord = vec2(tex_coord.x, 1 - tex_coord.y);\n"
 "}\0";
 
-//Códifo fonte do Fragment Shader (em GLSL): ainda hardcoded
+//CÃ³difo fonte do Fragment Shader (em GLSL): ainda hardcoded
 const GLchar* fragmentShaderSource = "#version 450\n"
 "in vec4 finalColor;\n"
 "in vec2 texCoord;\n"
@@ -74,19 +75,19 @@ const GLchar* fragmentShaderSource = "#version 450\n"
 
 bool rotateX=false, rotateY=false, rotateZ=false;
 
-// Adicionadas as variáveis para possibilitar o deslocamento nos três eixos, bem como a alteração da escala
+// Adicionadas as variÃ¡veis para possibilitar o deslocamento nos trÃªs eixos, bem como a alteraÃ§Ã£o da escala
 
 float posX = 0.0f, posY = 0.0f, posZ = 0.0f, scale = 1.0f;
 
-// Função MAIN
+// FunÃ§Ã£o MAIN
 int main()
 {
-	// Inicialização da GLFW
+	// InicializaÃ§Ã£o da GLFW
 	glfwInit();
 
-	//Muita atenção aqui: alguns ambientes não aceitam essas configurações
-	//Você deve adaptar para a versão do OpenGL suportada por sua placa
-	//Sugestão: comente essas linhas de código para desobrir a versão e
+	//Muita atenÃ§Ã£o aqui: alguns ambientes nÃ£o aceitam essas configuraÃ§Ãµes
+	//VocÃª deve adaptar para a versÃ£o do OpenGL suportada por sua placa
+	//SugestÃ£o: comente essas linhas de cÃ³digo para desobrir a versÃ£o e
 	//depois atualize (por exemplo: 4.5 com 4 e 5)
 	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
@@ -97,27 +98,27 @@ int main()
 //	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	//#endif
 
-	// Criação da janela GLFW
+	// CriaÃ§Ã£o da janela GLFW
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ola 3D -- Vitor Zillig", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
-	// Fazendo o registro da função de callback para a janela GLFW
+	// Fazendo o registro da funÃ§Ã£o de callback para a janela GLFW
 	glfwSetKeyCallback(window, key_callback);
 
-	// GLAD: carrega todos os ponteiros d funções da OpenGL
+	// GLAD: carrega todos os ponteiros d funÃ§Ãµes da OpenGL
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 
 	}
 
-	// Obtendo as informações de versão
+	// Obtendo as informaÃ§Ãµes de versÃ£o
 	const GLubyte* renderer = glGetString(GL_RENDERER); /* get renderer string */
 	const GLubyte* version = glGetString(GL_VERSION); /* version as a string */
 	cout << "Renderer: " << renderer << endl;
 	cout << "OpenGL version supported " << version << endl;
 
-	// Definindo as dimensões da viewport com as mesmas dimensões da janela da aplicação
+	// Definindo as dimensÃµes da viewport com as mesmas dimensÃµes da janela da aplicaÃ§Ã£o
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
@@ -150,10 +151,10 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 
 
-	// Loop da aplicação - "game loop"
+	// Loop da aplicaÃ§Ã£o - "game loop"
 	while (!glfwWindowShouldClose(window))
 	{
-		// Checa se houveram eventos de input (key pressed, mouse moved etc.) e chama as funções de callback correspondentes
+		// Checa se houveram eventos de input (key pressed, mouse moved etc.) e chama as funÃ§Ãµes de callback correspondentes
 		glfwPollEvents();
 
 		// Limpa o buffer de cor
@@ -165,7 +166,7 @@ int main()
 
 		float angle = (GLfloat)glfwGetTime();
 		
-		// Adicionadas matrizes de translação e escala
+		// Adicionadas matrizes de translaÃ§Ã£o e escala
 		model = glm::mat4(1); 
 		model = glm::translate(model, glm::vec3(posX, posY, posZ));
 		model = glm::scale(model, glm::vec3(scale));
@@ -192,7 +193,7 @@ int main()
 		// Chamada de desenho - drawcall
 		// Poligono Preenchido - GL_TRIANGLES
 		
-		// Ativação do buffer de textura e realização do bind com o buffer
+		// AtivaÃ§Ã£o do buffer de textura e realizaÃ§Ã£o do bind com o buffer
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texID);
 
@@ -210,13 +211,13 @@ int main()
 	}
 	// Pede pra OpenGL desalocar os buffers
 	glDeleteVertexArrays(1, &VAO);
-	// Finaliza a execução da GLFW, limpando os recursos alocados por ela
+	// Finaliza a execuÃ§Ã£o da GLFW, limpando os recursos alocados por ela
 	glfwTerminate();
 	return 0;
 }
 
-// Função de callback de teclado - só pode ter uma instância (deve ser estática se
-// estiver dentro de uma classe) - É chamada sempre que uma tecla for pressionada
+// FunÃ§Ã£o de callback de teclado - sÃ³ pode ter uma instÃ¢ncia (deve ser estÃ¡tica se
+// estiver dentro de uma classe) - Ã‰ chamada sempre que uma tecla for pressionada
 // ou solta via GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
@@ -244,7 +245,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		rotateZ = true;
 	}
 
-	//Adicionados os comandos para a movimentação nos três eixos, além da alteração da escala por meio das teclas de "-" e "+"
+	//Adicionados os comandos para a movimentaÃ§Ã£o nos trÃªs eixos, alÃ©m da alteraÃ§Ã£o da escala por meio das teclas de "-" e "+"
 	if (key == GLFW_KEY_W && action == GLFW_PRESS) {
 		posY += 0.1f;
 	}
@@ -271,18 +272,18 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	}
 }
 
-//Esta função está basntante hardcoded - objetivo é compilar e "buildar" um programa de
-// shader simples e único neste exemplo de código
-// O código fonte do vertex e fragment shader está nos arrays vertexShaderSource e
-// fragmentShader source no iniçio deste arquivo
-// A função retorna o identificador do programa de shader
+//Esta funÃ§Ã£o estÃ¡ basntante hardcoded - objetivo Ã© compilar e "buildar" um programa de
+// shader simples e Ãºnico neste exemplo de cÃ³digo
+// O cÃ³digo fonte do vertex e fragment shader estÃ¡ nos arrays vertexShaderSource e
+// fragmentShader source no iniÃ§io deste arquivo
+// A funÃ§Ã£o retorna o identificador do programa de shader
 int setupShader()
 {
 	// Vertex shader
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
-	// Checando erros de compilação (exibição via log no terminal)
+	// Checando erros de compilaÃ§Ã£o (exibiÃ§Ã£o via log no terminal)
 	GLint success;
 	GLchar infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -295,7 +296,7 @@ int setupShader()
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
-	// Checando erros de compilação (exibição via log no terminal)
+	// Checando erros de compilaÃ§Ã£o (exibiÃ§Ã£o via log no terminal)
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 	if (!success)
 	{
@@ -319,7 +320,7 @@ int setupShader()
 	return shaderProgram;
 }
 
-// Adição da função "loadSimpleOBJ"
+// AdiÃ§Ã£o da funÃ§Ã£o "loadSimpleOBJ"
 int loadSimpleOBJ(string filepath, int& nVerts, glm::vec3 color)
 {
 	vector <glm::vec3> vertices;
@@ -349,7 +350,7 @@ int loadSimpleOBJ(string filepath, int& nVerts, glm::vec3 color)
 			istringstream ssline(line);
 			ssline >> word;
 			//cout << word << " ";
-			//Mapeando as posições
+			//Mapeando as posiÃ§Ãµes
 			if (word == "v")
 			{
 				glm::vec3 v;
@@ -420,25 +421,25 @@ int loadSimpleOBJ(string filepath, int& nVerts, glm::vec3 color)
 	inputFile.close();
 	GLuint VBO, VAO;
 	nVerts = vbuffer.size() / 11; // 3 pos + 3 cor + 3 normal + 2 texcoord
-	//Geração do identificador do VBO
+	//GeraÃ§Ã£o do identificador do VBO
 	glGenBuffers(1, &VBO);
-	//Faz a conexão (vincula) do buffer como um buffer de array
+	//Faz a conexÃ£o (vincula) do buffer como um buffer de array
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//Envia os dados do array de floats para o buffer da OpenGl
 	glBufferData(GL_ARRAY_BUFFER, vbuffer.size() * sizeof(GLfloat), vbuffer.data(), GL_STATIC_DRAW);
-	//Geração do identificador do VAO (Vertex Array Object)
+	//GeraÃ§Ã£o do identificador do VAO (Vertex Array Object)
 	glGenVertexArrays(1, &VAO);
-	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vÃ©rtices
 	// e os ponteiros para os atributos 
 	glBindVertexArray(VAO);
 	//Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando: 
-	// Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
+	// LocalizaÃ§Ã£o no shader * (a localizaÃ§Ã£o dos atributos devem ser correspondentes no layout especificado no vertex shader)
 	// Numero de valores que o atributo tem (por ex, 3 coordenadas xyz) 
 	// Tipo do dado
-	// Se está normalizado (entre zero e um)
+	// Se estÃ¡ normalizado (entre zero e um)
 	// Tamanho em bytes 
 	// Deslocamento a partir do byte zero 
-	//Atributo posição (x, y, z)
+	//Atributo posiÃ§Ã£o (x, y, z)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 	//Atributo cor (r, g, b)
@@ -447,28 +448,28 @@ int loadSimpleOBJ(string filepath, int& nVerts, glm::vec3 color)
 	//Atributo coordenada de textura (s, t)
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(2);
-	//Atributo normal do vértice (x, y, z)
+	//Atributo normal do vÃ©rtice (x, y, z)
 	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(8 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(3);
 
-	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
-	// atualmente vinculado - para que depois possamos desvincular com segurança
+	// Observe que isso Ã© permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vÃ©rtice 
+	// atualmente vinculado - para que depois possamos desvincular com seguranÃ§a
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
+	// Desvincula o VAO (Ã© uma boa prÃ¡tica desvincular qualquer buffer ou array para evitar bugs medonhos)
 	glBindVertexArray(0);
 	return VAO;
 }
 
-// Adição da função "loadTexture"
+// AdiÃ§Ã£o da funÃ§Ã£o "loadTexture"
 int loadTexture(string path)
 {
 	GLuint texID;
 
-	// Gera o identificador da textura na memória 
+	// Gera o identificador da textura na memÃ³ria 
 	glGenTextures(1, &texID);
 	glBindTexture(GL_TEXTURE_2D, texID);
 
-	//Ajusta os parâmetros de wrapping e filtering
+	//Ajusta os parÃ¢metros de wrapping e filtering
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
@@ -502,7 +503,7 @@ int loadTexture(string path)
 	return texID;
 }
 
-// Função que lê o arquivo passado e retorna o nome da textura
+// FunÃ§Ã£o que lÃª o arquivo passado e retorna o nome da textura
 string getTextureName(string filepath)
 {
 
